d4fe63abc2c42cbf44a42759e3aa544ab2849ba1
Eldon <eldo0001@student.monash.edu>
2020-10-21 23:03:23 +0700

    initial

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -13,43 +13,43 @@ export class DatabaseService {
   constructor(private http: HttpClient) { }
 
   getActors() {
-    return this.http.get("http://localhost:8080/actors");
+    return this.http.get("/actors");
   }
   getActor(id: string) {
-    let url = "http://localhost:8080/actors/" + id;
+    let url = "/actors/" + id;
     return this.http.get(url);
   }
   createActor(data) {
-    return this.http.post("http://localhost:8080/actors", data, httpOptions);
+    return this.http.post("/actors", data, httpOptions);
   }
   updateActor(id, data) {
-    let url = "http://localhost:8080/actors/" + id;
+    let url = "/actors/" + id;
     return this.http.put(url, data, httpOptions);
   }
   deleteActor(id) {
-    let url = "http://localhost:8080/actors/" + id;
+    let url = "/actors/" + id;
     return this.http.delete(url, httpOptions);
   }
   getMovies(){
-    return this.http.get("http://localhost:8080/movies");
+    return this.http.get("/movies");
   }
   createMovie(data) {
-    return this.http.post("http://localhost:8080/movies", data, httpOptions);
+    return this.http.post("/movies", data, httpOptions);
   }
   deleteMovie(id) {
-    let url = "http://localhost:8080/movies/" + id;
+    let url = "/movies/" + id;
     return this.http.delete(url, httpOptions);
   }
   deleteMovieBefore(delyear){
-    let url= "http://localhost:8080/deletemovies/"+ delyear;
+    let url= "/deletemovies/"+ delyear;
     return this.http.delete(url, httpOptions);
   }
   addMovieActor(mid,aid,data){
-    let url="http://localhost:8080/movies/actors/"+mid+"/"+aid;
+    let url="/movies/actors/"+mid+"/"+aid;
     return this.http.post(url,data,httpOptions);
   }
   getActorAfter(){
-    let url="http://localhost:8080/actorsafter/"+2000;
+    let url="/actorsafter/"+2000;
     return this.http.get(url);
   }
 }
